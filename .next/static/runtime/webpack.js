7b86d5fd100eea6ff0271865751ff06b0f45b7b2
Marcin Swornowski <swor71@gmail.com>
2018-11-05 19:22:37 +0100

    Add top-of-the-page layout with styled components

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1ca385eda2f1d782cf51";
+/******/ 	var hotCurrentHash = "e134767bfb5c22061780";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
