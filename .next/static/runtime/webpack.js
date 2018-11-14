8621d31a7b9d54abbe90c0cb85b2767041e0afc1
Marcin Swornowski <swor71@gmail.com>
2018-11-14 20:44:32 +0100

    Add delete item query and update the apollo cache

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "11785534670f9efa7938";
+/******/ 	var hotCurrentHash = "402073e7206daa797d29";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
