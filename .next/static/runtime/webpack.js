3da3ca25782cc07738f4810976bf70f927153a78
Marcin Swornowski <swor71@gmail.com>
2018-11-28 18:35:11 +0100

    Add single item component / page for viewing item details

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "402073e7206daa797d29";
+/******/ 	var hotCurrentHash = "f9a989a9a5e12279ec04";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
